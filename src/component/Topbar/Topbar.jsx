c90112c1fc107726c06215d213fb0fcc045b326b
Yesarib <yesari_yesari@hotmail.com>
2023-02-21 00:16:48 +0300

    Topbar

--- src/component/Topbar/Topbar.jsx
+++ src/component/Topbar/Topbar.jsx
@@ -5,36 +5,13 @@ import Navbar from 'react-bootstrap/Navbar';
 const Topbar = () => {
   return (
     <>
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg="secondary" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-      <br />
-      <Navbar bg="primary" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-
-      <br />
-      <Navbar bg="light" variant="light">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Navbar.Brand href="#home">FSB</Navbar.Brand>
+          <Nav className="d-flex align-items-center justify-content-center">
+            <Nav.Link href="#home">Ana Sayfa</Nav.Link>
+            <Nav.Link href="#features">Hakkında</Nav.Link>
+            <Nav.Link href="#pricing">İletişim</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
