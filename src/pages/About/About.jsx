69ed467bb9d3d88a7dc5f4bc0771d764caeb25b9
Yesarib <yesari_yesari@hotmail.com>
2023-02-24 20:58:51 +0300

    About page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,11 +1,55 @@
 import React from 'react'
 import Middle from '../../components/Middle/Middle'
-import Footer from '../../components/Footer/Footer'
+import { Box } from '@mui/material'
 
 const About = () => {
   return (
     <div>
-      <Middle />
+      <Middle  />
+      <hr  style={{marginTop:'75px'}}/>
+      <Box className='justify-content-center d-flex'>
+        <div>
+          <p style={{marginTop:'50px',alignItems:'center'}}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                  Porro, perferendis alias! Ab hic sequi eveniet! Voluptatum nam dolore voluptas saepe,
+                  sapiente voluptate modi iste iure nemo nobis necessitatibus, id quidem?
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                  Porro, perferendis alias! Ab hic sequi eveniet! Voluptatum nam dolore voluptas saepe,
+                  sapiente voluptate modi iste iure nemo nobis necessitatibus, id quidem?
+          </p>
+          <p style={{marginTop:'50px',alignItems:'center'}}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                  Porro, perferendis alias! Ab hic sequi eveniet! Voluptatum nam dolore voluptas saepe,
+                  sapiente voluptate modi iste iure nemo nobis necessitatibus, id quidem?
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                  Porro, perferendis alias! Ab hic sequi eveniet! Voluptatum nam dolore voluptas saepe,
+                  sapiente voluptate modi iste iure nemo nobis necessitatibus, id quidem?
+          </p>
+        </div>
+        <img src="assets/kaba-insaat.jpg" alt="kaba-insaat" />
+      </Box>
+      <hr  style={{marginTop:'75px'}}/>
+      <Box className='justify-content-center d-flex'>
+        <div>
+          <p style={{marginTop:'50px',alignItems:'center'}}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                  Porro, perferendis alias! Ab hic sequi eveniet! Voluptatum nam dolore voluptas saepe,
+                  sapiente voluptate modi iste iure nemo nobis necessitatibus, id quidem?
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                  Porro, perferendis alias! Ab hic sequi eveniet! Voluptatum nam dolore voluptas saepe,
+                  sapiente voluptate modi iste iure nemo nobis necessitatibus, id quidem?
+          </p>
+          <p style={{marginTop:'50px',alignItems:'center'}}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                  Porro, perferendis alias! Ab hic sequi eveniet! Voluptatum nam dolore voluptas saepe,
+                  sapiente voluptate modi iste iure nemo nobis necessitatibus, id quidem?
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                  Porro, perferendis alias! Ab hic sequi eveniet! Voluptatum nam dolore voluptas saepe,
+                  sapiente voluptate modi iste iure nemo nobis necessitatibus, id quidem?
+          </p>
+        </div>
+        <img src="assets/indir.jpg" alt="kaba-insaat" />
+      </Box>
     </div>
   )
 }
